db8b6c3d5e54508fd79fffb489824d0214e43243
Diwakar Singh <101459162+itsdiwakarsingh@users.noreply.github.com>
2026-01-26 06:41:50 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 
+import Hero from './sections/Hero.jsx';
+import Navbar from "./sections/Navbar.jsx";
+
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
     return (
         <main>
-            <div>
-                <h1 className="text-3xl text-pink">Welcome to the world of GTA VI and GSAP</h1>
-            </div>
+            <Navbar />
+            <Hero />
         </main>
     )
 }
---
+++ src/sections/ComingSoon.jsx
@@ -0,0 +1,21 @@
+const ComingSoon = () => {
+    return (
+        <section className='entrance-message'>
+            <div className="h-full col-center gap-10">
+                <img src="/images/logo.webp" alt="logo" className='entrance-logo' />
+                <div className="text-wrapper">
+                    <h3 className="gradient-title">
+                        Comming <br /> XXX XXth <br /> 202X
+                    </h3>
+                </div>
+                <div className="flex-center gap-10">
+                    <img src="/images/ps-logo.svg" className='md:w-32 w-20' alt="PS-logo" />
+                    <img src="/images/x-logo.svg" className='md:w-52 w-40' alt="xbox-logo" />
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default ComingSoon
