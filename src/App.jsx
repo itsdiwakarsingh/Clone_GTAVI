d038ac63ee580df8e20ce8b357e51d62b5a0c8dd
Diwakar Singh <101459162+itsdiwakarsingh@users.noreply.github.com>
2026-01-31 23:20:21 +0530

    jason section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 
-import Hero from './sections/Hero.jsx';
+import Hero from "./sections/Hero.jsx";
 import Navbar from "./sections/Navbar.jsx";
 import FirstVideo from "./sections/FirstVideo.jsx";
+import Jason from "./sections/Jason.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
-    return (
-        <main>
-            <Navbar />
-            <Hero />
-            <FirstVideo />
-        </main>
-    )
-}
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <FirstVideo />
+      <Jason />
+    </main>
+  );
+};
 
-export default App 
+export default App;
